2ed9b6f6111762da5188b9228a7657d808ae8325
Mnemonica <hipek1983@me.com>
2023-08-25 16:16:53 +0100

    Added dots (just a test)

--- ArrayAndArrayList.java
+++ ArrayAndArrayList.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 
 /**
- * Class with methods for working with and manipulating Arrays and ArrayLists. 
+ * Class with methods for working with and manipulating Arrays and ArrayLists..
  */
 public class ArrayAndArrayList {
 
